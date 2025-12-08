d84ed823a6639c1b7694a1e14ae20174bb8b15f2
majid-tech <majidmohammed044@gmail.com>
2025-12-08 10:31:56 +0000

    complete webpage animation|menu

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,6 +1,12 @@
 const menuBtn = document.getElementById('menu');
 const linkList = document.querySelector('.link-list');
+const closeBtn = document.createElement('span')
+
+
 
 menuBtn.addEventListener('click', () => {
     linkList.classList.toggle('open');
+    menuBtn.style.display = 'none'
 });
+
+
